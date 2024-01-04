1a0a685be78dae260b2b40fb9e2cecefede9a282
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-01-04 14:58:39 +0300

    add mock grpc clients

--- app/reviews/usecase/review.go
+++ app/reviews/usecase/review.go
@@ -3,6 +3,8 @@ package reviewusecase
 import (
 	"kinopoisk/app/dto"
 	"kinopoisk/app/entity"
+	review "kinopoisk/service_review/proto"
+	"sync"
 )
 
 type ReviewUseCase interface {
@@ -10,4 +12,32 @@ type ReviewUseCase interface {
 	NewReview(newReview *dto.ReviewDTO, filmID, userID uint64) (*entity.Review, error)
 	DeleteReview(reviewID, userID uint64) (bool, error)
 	UpdateReview(reviewToUpdate *dto.ReviewDTO, reviewID, userID uint64) (*entity.Review, error)
+}
+
+type ReviewGRPCClient struct {
+	mu         *sync.RWMutex
+	grpcClient review.ReviewMakerClient
+}
+
+func NewReviewGRPCClient(grpcClient review.ReviewMakerClient) *ReviewGRPCClient {
+	return &ReviewGRPCClient{
+		mu:         &sync.RWMutex{},
+		grpcClient: grpcClient,
+	}
+}
+
+func (r *ReviewGRPCClient) GetFilmReviews(filmID uint64) ([]*entity.Review, error) {
+	return nil, nil
+}
+
+func (r *ReviewGRPCClient) NewReview(newReview *dto.ReviewDTO, filmID, userID uint64) (*entity.Review, error) {
+	return nil, nil
+}
+
+func (r *ReviewGRPCClient) DeleteReview(reviewID, userID uint64) (bool, error) {
+	return false, nil
+}
+
+func (r *ReviewGRPCClient) UpdateReview(reviewToUpdate *dto.ReviewDTO, reviewID, userID uint64) (*entity.Review, error) {
+	return nil, nil
 }
--- app/users/usecase/user.go
+++ app/users/usecase/user.go
@@ -1,6 +1,10 @@
 package userusecase
 
-import "kinopoisk/app/entity"
+import (
+	"kinopoisk/app/entity"
+	auth "kinopoisk/service_auth/proto"
+	"sync"
+)
 
 type UserUseCase interface {
 	Login(username, password string) (*entity.User, error)
@@ -8,4 +12,36 @@ type UserUseCase interface {
 	CreateSession(user *entity.User) (string, error)
 	GetSession(token string) (*entity.Session, error)
 	DeleteSession(token string) (bool, error)
+}
+
+type AuthGRPCClient struct {
+	mu         *sync.RWMutex
+	grpcClient auth.AuthMakerClient
+}
+
+func NewAuthGRPCClient(grpcClient auth.AuthMakerClient) *AuthGRPCClient {
+	return &AuthGRPCClient{
+		mu:         &sync.RWMutex{},
+		grpcClient: grpcClient,
+	}
+}
+
+func (a *AuthGRPCClient) Login(username, password string) (*entity.User, error) {
+	return nil, nil
+}
+
+func (a *AuthGRPCClient) Register(username, password string) (*entity.User, error) {
+	return nil, nil
+}
+
+func (a *AuthGRPCClient) CreateSession(user *entity.User) (string, error) {
+	return "", nil
+}
+
+func (a *AuthGRPCClient) GetSession(token string) (*entity.Session, error) {
+	return nil, nil
+}
+
+func (a *AuthGRPCClient) DeleteSession(token string) (bool, error) {
+	return false, nil
 }
