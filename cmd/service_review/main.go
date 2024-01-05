cee24f986c0d2d43d9fa7c5911c73c1e872ed318
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-01-05 16:56:15 +0300

    add main to review service

--- cmd/service_review/main.go
+++ cmd/service_review/main.go
@@ -1,5 +1,88 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	review "kinopoisk/service_review/proto"
+	reviewservicerepo "kinopoisk/service_review/repo/mysql"
+	reviewserviceusecse "kinopoisk/service_review/usecase"
+	"log"
+	"net"
+	"os"
+	"time"
+)
 
+const (
+	maxDBConnections  = 10
+	maxPingDBAttempts = 60
+)
+
+func openMySQLConnection() (*sql.DB, error) {
+	dsn := "root:"
+	mysqlPassword := os.Getenv("pass")
+	dsn += mysqlPassword
+	dsn += "@tcp(mysql:3306)/golang?"
+	dsn += "&charset=utf8"
+	dsn += "&interpolateParams=true"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(maxDBConnections)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	attemptsNumber := 0
+	for range ticker.C {
+		err = db.Ping()
+		attemptsNumber++
+		if err == nil {
+			break
+		}
+		if attemptsNumber == maxPingDBAttempts {
+			return nil, err
+		}
+	}
+	return db, nil
+}
+
+func main() {
+	zapLogger, err := zap.NewProduction()
+	if err != nil {
+		log.Printf("error in logger start")
+		return
+	}
+	logger := zapLogger.Sugar()
+	defer func() {
+		err = logger.Sync()
+		if err != nil {
+			log.Printf("error in logger sync")
+		}
+	}()
+	mySQLDb, err := openMySQLConnection()
+	if err != nil {
+		logger.Errorf("error in connection to mysql: %s", err)
+		return
+	}
+	defer func() {
+		err = mySQLDb.Close()
+		if err != nil {
+			logger.Errorf("error in close connection to mysql: %s", err)
+		}
+	}()
+
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		logger.Fatalf("can not listen port 8081: %s", err)
+	}
+	server := grpc.NewServer()
+
+	reviewRepo := reviewservicerepo.NewReviewRepoMySQL(mySQLDb, logger)
+
+	review.RegisterReviewMakerServer(server, reviewserviceusecse.NewReviewGRPCServer(reviewRepo))
+	logger.Info("starting server at :8081")
+	err = server.Serve(lis)
+	if err != nil {
+		logger.Fatalf("error in serving server on port 8081 %s", err)
+	}
 }
