d07e8ef4f8c4e905e2982fa5235a802c1f1e77d4
ilyushkaaa <ilyazhilenkov2003@gmail.com>
2024-01-05 03:20:08 +0300

    add ratelimiter implementation

--- app/ratelimiter/repo/redis/ratelimiter.go
+++ app/ratelimiter/repo/redis/ratelimiter.go
@@ -1,17 +1,43 @@
 package ratelimiterrepo
 
-import "github.com/gomodule/redigo/redis"
+import (
+	"github.com/gomodule/redigo/redis"
+	"go.uber.org/zap"
+)
 
 type RateLimiterRepo interface {
-	CheckRateLimitRepo(userAddr string) bool
+	CheckRateLimitRepo(userAddr string, minTime int64) int
+	AddRateRepo(userAddr string, currentTime int64)
 }
 
 type RateLimiterRepoRedis struct {
 	RedisConn redis.Conn
+	logger    *zap.SugaredLogger
 }
 
-func NewRateLimiterRepoRedis(redisConn redis.Conn) *RateLimiterRepoRedis {
+func NewRateLimiterRepoRedis(redisConn redis.Conn, logger *zap.SugaredLogger) *RateLimiterRepoRedis {
 	return &RateLimiterRepoRedis{
 		RedisConn: redisConn,
+		logger:    logger,
+	}
+}
+
+func (rl *RateLimiterRepoRedis) CheckRateLimitRepo(userAddr string, minTime int64) int {
+	count, err := redis.Int(rl.RedisConn.Do("ZCOUNT", userAddr, minTime, "+inf"))
+	if err != nil {
+		rl.logger.Errorf("error in count requests in ratelimiter")
+		return 0
+	}
+	_, err = rl.RedisConn.Do("ZREMRANGEBYSCORE", userAddr, "-inf", minTime)
+	if err != nil {
+		rl.logger.Errorf("error in deleting old  requests in ratelimiter")
+	}
+	return count
+}
+
+func (rl *RateLimiterRepoRedis) AddRateRepo(userAddr string, currentTime int64) {
+	_, err := rl.RedisConn.Do("ZADD", userAddr, "", currentTime)
+	if err != nil {
+		rl.logger.Errorf("error in adding request in ratelimiter")
 	}
 }
--- app/ratelimiter/usecase/ratelimiter.go
+++ app/ratelimiter/usecase/ratelimiter.go
@@ -3,6 +3,7 @@ package ratelimiterusecase
 import (
 	ratelimiterrepo "kinopoisk/app/ratelimiter/repo/redis"
 	"sync"
+	"time"
 )
 
 type RateLimiterUseCase interface {
@@ -19,4 +20,17 @@ func NewFilmUseCaseStruct(repo ratelimiterrepo.RateLimiterRepo) *RateLimiterUseC
 		mu:              &sync.RWMutex{},
 		RateLimiterRepo: repo,
 	}
+}
+
+func (rl *RateLimiterUseCaseStruct) CheckRateLimit(userAddr string) bool {
+	currentTimeMillis := time.Now().UnixNano() / int64(time.Millisecond)
+	rl.mu.RLock()
+	numOfRequests := rl.RateLimiterRepo.CheckRateLimitRepo(userAddr, currentTimeMillis-2000)
+	rl.mu.RUnlock()
+	var canMakeRequest = false
+	if numOfRequests < 3 {
+		canMakeRequest = true
+		rl.RateLimiterRepo.AddRateRepo(userAddr, currentTimeMillis)
+	}
+	return canMakeRequest
 }
